1e3306932db2924b60001ea181aea1f6e0930183
Andrew Dixon <andrew.dixon@lendstreet.com>
2018-06-21 14:07:15 -0700

    animate background

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -10,12 +10,18 @@ export class Game extends React.Component {
     this.canvas = null;
     this.background = new  Image();
     this.background.src = require("../../../images/background.png");
-    this.background.onload = this.draw.bind(this)
+    this.draw = this.draw.bind(this)
+    this.background.onload = this.draw;
+    this.y = 0;
   }
 
   draw() {
+    window.requestAnimationFrame(this.draw);
     const ctx = this.canvas.getContext('2d');
-    ctx.drawImage(this.background, 0, 0);
+    ctx.drawImage(this.background, 0, this.y);
+    ctx.drawImage(this.background, 0, this.y - this.canvas.height);
+    this.y += 1;
+    if (this.y >= 120) this.y = 120 - this.background.height;
   }
 
   render() { return <canvas id="background" ref={e => this.canvas = e} /> }
