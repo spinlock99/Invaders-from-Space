671fc32296e54057a712c5d2fef0d714c40d959a
Andrew Dixon <andrew.dixon@lendstreet.com>
2018-06-25 15:20:16 -0700

    init -> onload

--- src/canvas/image-repository.js
+++ src/canvas/image-repository.js
@@ -1,15 +1,15 @@
-export default function ImageRepository(init) {
+export default function ImageRepository(onload) {
   this.imagesToLoad = 3;
 
   this.background = new Image();
   this.background.src = require("background.png");
-  this.background.onload = e => --this.imagesToLoad || init();
+  this.background.onload = e => --this.imagesToLoad || onload();
 
   this.ship = new Image();
   this.ship.src = require("spaceship.png");
-  this.ship.onload = e => --this.imagesToLoad || init();
+  this.ship.onload = e => --this.imagesToLoad || onload();
 
   this.laser = new Image();
   this.laser.src = require("laser.png");
-  this.laser.onload = e => --this.imagesToLoad || init();
+  this.laser.onload = e => --this.imagesToLoad || onload();
 }
