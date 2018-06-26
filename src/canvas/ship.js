47d49bc69bd8066748a9ba6929599d18f3c5f016
Andrew Dixon <andrew.dixon@lendstreet.com>
2018-06-26 15:44:42 -0700

    remove offset for ship on touch events

--- src/canvas/ship.js
+++ src/canvas/ship.js
@@ -2,27 +2,27 @@ import Drawable from "canvas/drawable";
 import Pool from "canvas/pool";
 
 function Ship() {
-  this.thumbHeight = 100;
   this.lasers = new Pool(40);
   this.leftGunOffset = 5;
   this.rightGunOffset = 33;
 
-
   this.nextX = null;
   this.nextY = null;
 
-  this.draw = () => {
-    this.element.context.clearRect(this.x, this.y, this.width, this.height);
-    this.x = this.nextX || this.x;
-    this.y = this.nextY || this.y;
-    this.element.context.drawImage(this.image, this.x, this.y);
-  }
+  this.draw = () =>
+    {
+      this.element.context.clearRect(this.x, this.y, this.width, this.height);
+      this.x = this.nextX || this.x;
+      this.y = this.nextY || this.y;
+      this.element.context.drawImage(this.image, this.x, this.y);
+    };
 
-  this.move = (x, y) => {
-    this.nextX = x - this.image.width / 2;
-    this.nextY = y - this.thumbHeight;
-    this.draw();
-  };
+  this.move = (x, y) =>
+    {
+      this.nextX = x;
+      this.nextY = y;
+      this.draw();
+    };
 
   this.fire = () => this.lasers.getTwo(this.x + this.leftGunOffset, this.x + this.rightGunOffset, this.y, 3);
 }
