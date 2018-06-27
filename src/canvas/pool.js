367e587313f37d2bdf08828cc460d59d1cf5aed2
Andrew Dixon <andrew.dixon@lendstreet.com>
2018-06-26 17:38:38 -0700

    linting

--- src/canvas/pool.js
+++ src/canvas/pool.js
@@ -6,53 +6,57 @@ export default function Pool(size) {
   this.size = size;
   this.pool = [];
 
-  this.init = (type, image) => {
-    for (let i = 0; i < this.size; i++) {
-      let object = null;
-      switch (type) {
-        case "laser":
-          object = new Laser();
-          object.type = "laser";
-          object.collidableWith = "enemy";
-          break;
-        case "enemy":
-          object = new Enemy();
-          break;
-        case "enemyLaser":
-          object = new EnemyLaser();
-          object.type = "enemyLaser";
-          object.collidableWith = "ship";
-          break;
+  this.init = (type, image) =>
+    {
+      for (let i = 0; i < this.size; i++) {
+        let object = null;
+        switch (type) {
+          case "laser":
+            object = new Laser();
+            object.type = "laser";
+            object.collidableWith = "enemy";
+            break;
+          case "enemy":
+            object = new Enemy();
+            break;
+          case "enemyLaser":
+            object = new EnemyLaser();
+            object.type = "enemyLaser";
+            object.collidableWith = "ship";
+            break;
+        }
+        object.init(0, 0, image);
+        this.pool[i] = object;
       }
-      object.init(0, 0, image);
-      this.pool[i] = object;
-    }
-  };
+    };
 
-  this.get = (x, y, speed) => {
-    if (!this.pool[this.size - 1].alive) {
-      this.pool[this.size - 1].spawn(x, y, speed);
-      this.pool.unshift(this.pool.pop());
-    }
-  };
+  this.animate = () =>
+    {
+      for (let i = 0; i < size; i++) {
+        if (!this.pool[i].alive) break;
 
-  this.getTwo = (x1, x2, y, speed) => {
-    if (!this.pool[this.size - 1].alive && !this.pool[this.size - 2].alive) {
-      this.get(x1, y, speed);
-      this.get(x2, y, speed);
-    }
-  };
-
-  this.animate = () => {
-    for (let i = 0; i < size; i++) {
-      if (!this.pool[i].alive) break;
-
-      if (this.pool[i].draw()) {
-        this.pool[i].clear();
-        this.pool.push(this.pool.splice(i, 1)[0]);
+        if (this.pool[i].draw()) {
+          this.pool[i].clear();
+          this.pool.push(this.pool.splice(i, 1)[0]);
+        }
       }
-    }
-  };
+    };
 
-  this.getPool = () => this.pool.filter(object => object.alive);
+    this.get = (x, y, speed) =>
+      {
+        if (!this.pool[this.size - 1].alive) {
+          this.pool[this.size - 1].spawn(x, y, speed);
+          this.pool.unshift(this.pool.pop());
+        }
+      };
+
+  this.getTwo = (x1, x2, y, speed) =>
+    {
+      if (!this.pool[this.size - 1].alive && !this.pool[this.size - 2].alive) {
+        this.get(x1, y, speed);
+        this.get(x2, y, speed);
+      }
+    };
+
+    this.getPool = () => this.pool.filter(object => object.alive);
 }
