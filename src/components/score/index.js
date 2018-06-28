f9fe7a59cfddcc68f5c29811d34bada2c26ee20b
Andrew Dixon <andrew.dixon@lendstreet.com>
2018-06-28 14:58:12 -0700

    Use ImmutableJS for Reducer

--- src/components/score/index.js
+++ src/components/score/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 
 export default connect(
-  state => ({ score: state.score })
-)(props =>
-  <div style={{
+  state => ({ score: state.get('score') })
+)(
+  props => <div style={styles.score}>SCORE: {props.score}</div>
+);
+
+const styles = {
+  score: {
     position: "absolute",
     top: "5px",
     right: "50px",
     color: "#FF7F00",
     fontFamily: "Helvetica, sans-serif",
     cursor: "default",
-  }}>SCORE: {props.score}</div>
-)
+  },
+};
--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,11 @@
-const init = { score: 0 };
+import Immutable from "immutable";
+
+const init = Immutable.fromJS({ score: 0 });
 
 export default function(state=init, action) {
   switch(action.type) {
     case "ENEMY_DESTROYED":
-      return { ...state, score: state.score + 10 }
+      return state.set('score', state.get('score') + 10);
     default:
       return state;
   }
