340eee61f6d26be14cf8eacc7a4702bd6707e6ec
Albert Itayev <sirjsx@gmail.com>
2019-01-20 00:11:20 +0100

    Setup Introduction component

--- src/components/introduction/index.js
+++ src/components/introduction/index.js
@@ -1,9 +1,47 @@
 import React from "react"
 import styles from "./styles"
+import Button from '@material-ui/core/Button';
+
+const slides = [
+  {
+    num: 1,
+    img: "https://i.imgur.com/78KHHjw.jpg",
+    text: [
+      "President Trump has won reelection and surprised the nation with the choice of a surprise running mate: Barack Obama.",
+      "The two meet in the Oval Office to discuss the threat that brought them together."
+    ]
+  }
+]
 
 const Introduction = props =>
   <div style={styles.background}>
-    Introduction Screen
+    <div style={styles.btnContainer}>
+      <StartIntro />
+    </div>
+
+    <div style={styles.slides}>
+      {slides.map(slide => (
+        <Slide key={slide.num} {...slide} />
+      ))}
+    </div>
+  </div>
+
+const StartIntro = () =>
+  <Button variant="contained" size="large">
+    Play Intro
+  </Button>
+
+const Slide = ({ img, text }) =>
+  <div>
+    <div style={styles.sceneContainer}>
+      <img src={img} style={styles.scene} />
+    </div>
+
+    <div className="sceneText" style={styles.text}>
+      {text.map((t, i) => (
+        <p key={i}>{t}</p>
+      ))}
+    </div>
   </div>
 
 export default Introduction
--- src/components/introduction/styles.js
+++ src/components/introduction/styles.js
@@ -1,8 +1,31 @@
 export default {
   background: {
     backgroundColor: "#000",
-    color: "#fff",
     width: "100%",
     height: "100%"
+  },
+  btnContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    display: "none"
+  },
+  slides: {
+    color: "#fff",
+  },
+  sceneContainer: {
+    textAlign: 'center',
+    maxHeight: "60vh"
+  },
+  scene: {
+    width: "90%",
+    padding: "15px",
+    boxSizing: "border-box"
+  },
+  text: {
+    fontSize: "14px",
+    margin: "15px",
+    maxHeight: "50vh"
   }
 }
